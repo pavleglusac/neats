629c3a66350e3845c59268f8f71daf5f8ba07681
Pavle Glusac <pavleg.19@gmail.com>
2021-01-29 00:04:19 +0100

    Debugging part 3

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def running_dinosaur():
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True)
+    app.run(debug=True, port=33507)
